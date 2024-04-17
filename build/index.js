2c3cae85506b7ad243f493ef6ae195e661b628b9
izure <izure@naver.com>
2024-04-17 10:48:17 +0900

    build: Convert module build output to CommonJS (cjs) and ECMAScript module (mjs) files

--- build/index.js
+++ build/index.js
@@ -1,32 +1,28 @@
-const path = require('path')
 const esbuild = require('esbuild')
-const { dtsPlugin } = require('esbuild-plugin-d.ts')
 
 const common = {
   target: 'esnext',
-  plugins: [
-    dtsPlugin()
-  ]
-}
-
-esbuild.build({
-  ...common,
   entryPoints: [
     { in: 'src/index.ts', out: 'index' },
     { in: 'src/Helpers/VertexEncoder.ts', out: 'Helpers/VertexEncoder' }
   ],
-  outdir: 'dist/cjs',
+  bundle: true
+}
+
+esbuild.build({
+  ...common,
   format: 'cjs',
-  bundle: true,
+  outdir: 'dist/cjs',
+  outExtension: {
+    '.js': '.cjs'
+  }
 })
 
 esbuild.build({
   ...common,
-  entryPoints: [
-    { in: 'src/index.ts', out: 'index' },
-    { in: 'src/Helpers/VertexEncoder.ts', out: 'Helpers/VertexEncoder' },
-  ],
-  outdir: 'dist/esm',
   format: 'esm',
-  bundle: true,
+  outdir: 'dist/esm',
+  outExtension: {
+    '.js': '.mjs'
+  }
 })
